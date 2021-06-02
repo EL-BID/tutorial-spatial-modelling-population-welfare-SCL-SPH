7d8625408944c72d64e90a72565948b9894b4a63
rsanchezavalos <r.sanchezavalos@gmail.com>
2021-06-02 12:21:27 -0500

    fix cross references

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
-sudo docker run -d -p 8889:8787 -e ROOT=TRUE  -e PASSWORD=geotest --name geo_3_5_1  -v $(pwd):/home/rstudio rocker/geospatial:3.5.1
-# 4.0.0,3.6.0 3.5.0 3.4.0 3.6.2
+
+sudo docker run -d -p 8889:8787 -e ROOT=TRUE  -e PASSWORD=geotest --name geo_3_5_1  -v $(pwd):/home/rstudio rocker/geospatial:4.0.0
 docker start geo_3_5_1
+
+# sudo docker run -d -p 8889:8787 -e ROOT=TRUE  -e PASSWORD=geotest --name geo_inla_bio -v $(pwd):/home/rstudio bioconductor/bioconductor_docker:devel
+# docker start geo_inla_bio
--- utils.R
+++ utils.R
@@ -16,51 +16,79 @@ dir_data=paste0(root_dir,project_dir)
 
 ##### Cran
 pk <- c("devtools", "stringr", "dplyr",
-        "rgdal", "car", "plotly",
-        "plm", "MatrixModels", "leaflet",
-        "plotly", "gpclib", "rgeos",
-        "leaflet.extras","Hmisc",
-        "parallel",  "qgraph", # "WGCNA",
-        "energy", # "Pigengene"
-        "tictoc", "velox", "RJSONIO", "kableExtra", "furrr") #
+        "car", "plotly", "leaflet", "plotly", 
+        "RJSONIO", "kableExtra", "furrr", "leaflet.extras",
+        "qgraph", "tictoc",  "energy",
+        "parallel", "WGCNA",
+        # "Pigengene",
+        "plm", "MatrixModels", 
+        "Hmisc","gpclib", "rgeos","rgdal", "velox"
+        ) #,
 install <- pk[!(pk %in% installed.packages()[,'Package'])]
 if(length(install)) install.packages(install)
 res <- lapply(pk, require, character.only = TRUE)
 if(Reduce(res, f = sum)/length(pk) < 1) stop('Some packages could not be loaded.')
 
-library("Pigengene")
 
-
+install_github("hunzikp/velox")
 
 #####################
 ##### Source Install before loading
 ##### TODO(rsanchezavalos) install in dockerfile
-#####################
-# ##### Source & dev
-# # GIT - devtools
-# # --------
-# #install.packages("de   vtools")
-# install.packages("plm") # https://cran.r-project.org/web/packages/plm/vignettes/plmPackage.html
+####################
+##### Source & dev
+# GIT - devtools
+# --------
+#install.packages("de   vtools")
+#install.packages("plm") # https://cran.r-project.org/web/packages/plm/vignettes/plmPackage.html
 
-# # INLA
-# # --------
-# TODO(rsanchezavalos) # freeze INLA version
+
+#####################################################
+# INLA
+# --------
+#TODO(rsanchezavalos) # freeze INLA version
 # install.packages("INLA", repos=c(getOption("repos"),
 #                                   INLA="https://inla.r-inla-download.org/R/stable"),
 #                   dep=TRUE)
-# library("INLA")
 
-# #INLA:::inla.dynload.workaround()
-# This function is replaced by: inla.binary.install() in new R - use -> 
-# * Install file [https://inla.r-inla-download.org/Linux-builds/./CentOS Linux-6 (Core)/Version_21.02.23/64bit.tgz]
+# R > 3.5.0
+#INLA:::inla.dynload.workaround()
+#This function is replaced by: inla.binary.install() in new R - use ->
+#* Install file [https://inla.r-inla-download.org/Linux-builds/./CentOS Linux-6 (Core)/Version_21.02.23/64bit.tgz]
 
-# # inla.binary.install()
-# # BiocManager
-# # --------
-# # R > 3.5.0
+# inla.binary.install()
+#library("INLA")
+
+#####################################################
+# Install BiocManager - biocLite
+# --------
+# R < 3.5.0
+#source("https://bioconductor.org/biocLite.R")
+
+# R > 3.5.0
 # if (!requireNamespace("BiocManager", quietly = TRUE))
 #   install.packages("BiocManager")
-# # BiocManager::install(c("biocLite"))
-# # R < 3.5.0
-# #source("https://bioconductor.org/biocLite.R")
-# biocLite("Pigengene")
+
+# update.packages()
+# source("https://bioconductor.org/biocLite.R")
+# biocLite()
+
+
+# Bioconductor version 3.8 (BiocManager 1.30.4), R 3.5.1 (2018-07-02)
+# BiocManager::install(c("biocLite"))
+
+#####################################################
+
+# R > 4.0.0
+# install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+# 
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#         install.packages("BiocManager")
+# BiocManager::install(c("graph", "Rgraphviz"), dep=TRUE)
+# BiocManager::install(c("Pigengene"))
+
+
+library("graph")
+library("Rgraphviz")
+library("Pigengene")
+
